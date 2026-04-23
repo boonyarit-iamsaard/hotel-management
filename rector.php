ee3d66392ba36537cf616314b95499f44391662c
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-04-23 20:01:11 +0700

    chore: configure larastan and rector

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/tests',
+    ])
+    ->withoutParallel()
+    ->withSets([
+        LevelSetList::UP_TO_PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::TYPE_DECLARATION,
+    ]);
